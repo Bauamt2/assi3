ec03d4cadb3dadad2d8e50b722ee94e03d6bfaa7
jn <skjamueh>
2018-12-06 17:46:37 +0100

    31

--- main_s.c
+++ main_s.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 //
-int main() {
+int main(int argc, char **argv) {// -e 1 2 3 4 5 6 7 -n 199(ergebnis) -t 23000 (port)
+    int e[7];
+    int erg;
+    int port;
+
+
+
+
+
     printf("HALLO ICH BIN DER SERVER\n");
     return 0;
 }
