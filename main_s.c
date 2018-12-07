bb94574fccb595c20231f48bdd162a5c286b4878
jn <skjamueh>
2018-12-07 06:17:22 +0100

    31

--- main_s.c
+++ main_s.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-//
-void efuellen(int e[],char* estring){
-    int i=0;
-while(estring != '\0' && i < 7){
-    if(*estring != ' '){
-        e[i] = *estring;
-        printf("schreibe an i: %d wert: %d\n",i,*estring);
-        i++;
-    }
-    estring++;
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-}
+#define NACHRICHTENLAENGE 100
+//
+int efuellen(int e[],char* estring){
+    char* temp[7];
+    int warn =0;
+
+    for(int i = 0;i<7;i++) {
+        while (*estring == ' ') {
+            estring++;
+        }
+        temp[i] = estring;
+        while (*estring != ' ') {
+            estring++;
+        }
+    }//Hiernach zeigt temp auf den Beginn jedes Zahlenstrings
+
+    for(int i = 0;i<7;i++){
+        e[i] = atoi(temp[i]);
+        if(e[i] < 1 || e[i] > 100){
+            printf("Parameter e fehlerhaft! Es müssen 7 Zahlen zwischen 1 und 100 durch Leerzeichen getrennt angegeben werden!\n");
+            return -1;
+
+        }
+    }
+
+return 1;
 }
 
 int main(int argc, char **argv) {// -e 1 2 3 4 5 6 7 -n 199(ergebnis) -t 23000 (port)
     int e[7];
     char* estring = NULL;
-    int erg = NULL;
-    int port = NULL;
+    int erg = 0;
+    uint16_t port = 0;
     int opt = 0;
-while((opt = getopt(argc,argv, "e:n:t:")) != -1){
+while((opt = getopt(argc,argv, "e:n:p:")) != -1){
     switch (opt){
-        case 't':
+        case 'p':
             port = atoi(optarg);
             break;
         case 'n':
@@ -38,19 +58,60 @@ while((opt = getopt(argc,argv, "e:n:t:")) != -1){
 printf("Port: %d\n",port);
 printf("Ergebnis: %d\n",erg);
 printf("Aufgabe: %s\n",estring);
-    efuellen(e, estring);//TODO IST MÜLL MOMENTAN
+if(efuellen(e, estring) < 1){
+    return -1;
+}
+if(erg < 1 || erg > 1000){
+    printf("Parameter n fehlerhaft! Der Wert muss zwischen 1 und 1000 liegen!\n");
+    return -1;
+}
+if(port < 1024 || port > 49151){
+    printf("Parameter p fehlerhaft! Der Wert muss zwischen 1024 und 49151 liegen!\n");
+    return -1;
+}
 for(int i=0;i<7;i++){
     printf("e: %d\n",e[i]);
 }
 
+    printf("Parameter OK\n");
+//Jetzt muss der server auf Verbindungen warten
 
+char *nachricht = "Willkommen client!";
 
-if(erg != NULL && port != NULL && estring != NULL ){//Alle Argumente wurden angegeben
-    printf("Kann jetzt starten\n");
-}else{
-    printf("Fehler in der Eingabe!");
-    return -1;
+struct sockaddr_in dest;
+struct sockaddr_in serv;
+int mysocket;
+socklen_t socksize = sizeof(struct sockaddr_in);
+
+memset(&serv, 0 ,sizeof(serv));
+serv.sin_family = AF_INET;
+serv.sin_addr.s_addr = htonl(INADDR_ANY);
+serv.sin_port = htons(port);
+
+mysocket = socket(AF_INET, SOCK_STREAM,0);
+
+bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr));
+
+int host =1;
+int ende = 0;
+while(host == 1 && ende == 0) {
+    listen(mysocket, 10);
+    fork();
+    //TODO: abbruchbedingung durch eingabe
 }
+    int consocket = accept(mysocket, (struct sockaddr *) &dest, &socksize);
+while(consocket){
+    printf("Verbindung von: %s\n",inet_ntoa(dest.sin_addr));
+    send(consocket, nachricht, strlen(nachricht),0);
+    close(consocket);
+    consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
+}
+
+close(mysocket);
+
+
+
+
 
     return 0;
 }
